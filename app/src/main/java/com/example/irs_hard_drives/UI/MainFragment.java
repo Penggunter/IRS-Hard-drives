aafdbc48fcb3d185fcea8e1cc03651162ab19f3b
shevi <shev.igor2002@yandex.ru>
2023-04-30 12:53:08 +0300

    Added activity description for item

---
+++ app/src/main/java/com/example/irs_hard_drives/UI/MainFragment.java
@@ -0,0 +1,23 @@
+package com.example.irs_hard_drives.UI;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+import com.example.irs_hard_drives.R;
+
+public class MainFragment extends Fragment {
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.activity_main, container, false);
+
+        return view;
+    }
+}
